94faad34abcdf7db2122aff1698cb21d8a12516a
נתנאל בשן <nbashan@g.jct.ac.il>
2021-04-29 16:56:45 +0300

    chore: moved configuration to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
 import setuptools
 
-setuptools.setup()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pypassafe",
+    version="0.0.1",
+    description="py-passafe - library for password management",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nbashan/py-passafe.git",
+    classifieres=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    packages=setuptools.find_packages(include=["pypassafe"]),
+    python_requires=">=3.8",
+    install_requires=[],
+)
