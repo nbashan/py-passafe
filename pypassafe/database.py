a5d1bb265cf9822955c00903146c9eb80bba0b26
Meir Klemfner <meirk251@outlook.com>
2021-05-03 14:50:36 +0300

    feat: add ability to choose db in EncryptedDB ctor

--- pypassafe/database.py
+++ pypassafe/database.py
@@ -1,4 +1,4 @@
-from typing import Optional, Callable, Any
+from typing import Optional, Callable, Any, Type
 
 class DataBase:
     def __init__(self, path: str) -> None:
@@ -34,14 +34,14 @@ class DecryptedDB:
         return EncryptedDB(db=self.db)
 
 class EncryptedDB:
-    def __init__(self, path: Optional[str] = None, db: Optional[DataBase] = None) -> None:
+    def __init__(self, db_type: Optional[Type[DataBase]] = None, path: Optional[str] = None, db: Optional[DataBase] = None) -> None:
         if db is not None:
             self.db = db
             return
-        if path is not None:
-            self.db = DataBase(path)
+        if path is not None and type is not None:
+            self.db = db_type(path)
             return
-        raise ValueError("EncryptedDB should get path or existing DataBase")
+        raise ValueError("EncryptedDB should get path and db type or existing DataBase")
 
     def decrypt(self, master: str) -> DecryptedDB:
         self.db.decrypt(master)
