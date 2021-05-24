c9ac0317ee9af22a63bc20d2fdbfa444ed5589d9
Meir Klemfner <meirk251@outlook.com>
2021-05-24 15:01:43 +0300

    chore: add sub-packages to setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
-    packages=["pypassafe"],
+    packages=setuptools.find_packages(include=["pypassafe", "pypassafe.*"]),
     python_requires=">=3.8",
     install_requires=[
         "pycryptodome",
